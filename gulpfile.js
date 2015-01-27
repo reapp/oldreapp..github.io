30686f45925a7674682d0aecfe63242e22237a68
natew <natewienert@gmail.com>
2015-01-26 19:50:58 -0800

    begin generate components and modules

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,43 @@ var wrap = require('gulp-wrap');
 var concat = require('gulp-concat');
 
 var buildDir = './gen';
-var outDir = './docs';
+var outDir = './pages';
 
 gulp.task('clean', function(cb) {
   return rimraf(buildDir, cb);
 });
 
-gulp.task('copy', ['clean'], function() {
+var packages = ['ui', 'pack', 'server', 'routes', 'component', 'platform', 'request', 'reducer', 'object-assign', 'raf-batching'];
+var packagePaths = packages.map(function(name) { return '../reapp-'+name+'/README.md' });
+
+gulp.task('modules', ['clean'], function() {
   return gulp
-    .src('../*/README.md')
+    .src(packagePaths, { base: '../' })
     .pipe(rename(function (path) {
       path.basename = path.dirname;
       path.dirname = '';
       path.extname = '.md';
     }))
     .pipe(markdown())
-    .pipe(
-      gulp.dest(buildDir)
-    );
-});
-
-gulp.task('make-pages', ['copy'], function() {
-  return gulp.src([buildDir + '/*.html'])
-    .pipe(concat('components.html'))
-    .pipe(wrap({src: './layout.html'}))
+    .pipe(concat('modules.html'))
+    .pipe(wrap({ src: './templates/page.html' }))
+    .pipe(wrap({ src: './templates/layout.html' }))
     .pipe(gulp.dest(outDir));
 });
 
-gulp.task('default', ['make-pages']);
+gulp.task('components', ['clean'], function() {
+  return gulp
+    .src('../reapp-ui/docs/*')
+    .pipe(rename(function (path) {
+      path.basename = path.dirname;
+      path.dirname = '';
+      path.extname = '.md';
+    }))
+    .pipe(markdown())
+    .pipe(concat('components.html'))
+    .pipe(wrap({ src: './templates/page.html' }))
+    .pipe(wrap({ src: './templates/layout.html' }))
+    .pipe(gulp.dest(outDir));
+});
+
+gulp.task('default', ['modules', 'components']);
